3735c9603cfe00877623e1d7473058616a9720e3
Abhishek Pabal <121388948+Abhi2890@users.noreply.github.com>
2024-01-02 21:19:48 +0530

    Update Simple Calculator .py

--- Simple Calculator .py
+++ Simple Calculator .py
@@ -1,1 +1,53 @@
+import tkinter as tk
+class Calculator(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Calculator")
+        self.geometry("300x400")
+        self.result_var = tk.StringVar()
+        self.create_widgets()
+
+    def create_widgets(self):
+        entry = tk.Entry(self, textvariable=self.result_var,bg="#8acaf2", font=('Arial', 24), bd=10, insertwidth=4, width=14,
+                         justify='right')
+        entry.grid(row=0, column=0, columnspan=4)
+
+        buttons = [
+            ('7', 1, 0), ('8', 1, 1), ('9', 1, 2), ('/', 1, 3),
+            ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('*', 2, 3),
+            ('1', 3, 0), ('2', 3, 1), ('3', 3, 2), ('-', 3, 3),
+            ('0', 4, 0), ('C', 4, 1), ('=', 4, 2), ('+', 4, 3),
+        ]
+
+        for (text, row, column) in buttons:
+            button = tk.Button(self, text=text, font=('Black', 18),bg="#8acaf2", command=lambda t=text: self.on_button_click(t))
+            button.grid(row=row, column=column, sticky='nsew')
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+        self.grid_rowconfigure(4, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+
+    def on_button_click(self, text):
+        if text == '=':
+            try:
+                result = eval(self.result_var.get())
+                self.result_var.set(result)
+            except Exception as e:
+                self.result_var.set("Error")
+        elif text == 'C':
+            self.result_var.set('')
+        else:
+            current_text = self.result_var.get()
+            current_text += text
+            self.result_var.set(current_text)
+
+if __name__ == "__main__":
+    app = Calculator()
+    app.mainloop()
 
